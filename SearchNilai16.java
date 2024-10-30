6903e63b29d367816293169e669903bb471604c8
zakariahaniya <zakalvha@gmail.com>
2024-10-30 16:24:56 +0700

    daspro jobsheet 9

--- SearchNilai16.java
+++ SearchNilai16.java
@@ -1,21 +1,31 @@
+import java.util.Scanner;
 /**
  * SearchNilai16
  */
 public class SearchNilai16 {
 
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
-
-        for (int i = 0; i < arrNilai.length; i++) {
-            if (key == arrNilai[i]) {
-                hasil = i;
-                break;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan banyaknya nilai yang akan diinput : ");
+        int length = sc.nextInt();
+        int[] nilaiMhs = new int[length];
+        int key, hasil = 0;
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
+            nilaiMhs[i] = sc.nextInt(); 
+        }
+        System.out.print("Masukkan nilai yang ingin dicari : ");
+        key = sc.nextInt();
+        System.out.println();
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            if (key == nilaiMhs[i]) {
+                hasil = i+1;
             }
         }
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
+        if (hasil == 0) {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        } else {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + hasil);
+        }
     }
 }
