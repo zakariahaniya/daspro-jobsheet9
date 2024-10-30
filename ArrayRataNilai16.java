6038b191cab1203e5fbcb52b663eb7561091034e
zakariahaniya <zakalvha@gmail.com>
2024-10-30 11:26:45 +0700

    daspro jobsheet 9

--- ArrayRataNilai16.java
+++ ArrayRataNilai16.java
@@ -6,17 +6,26 @@ public class ArrayRataNilai16 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int length = sc.nextInt();
+        int[] nilaiMhs = new int[length];
+        int lulus = 0, gagal = 0;
+        double totalLulus = 0, totalGagal = 0;
+        double rata2Lulus, rata2Gagal;
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
             nilaiMhs[i] = sc.nextInt();
+            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                lulus++;
+            } else {
+                totalGagal += nilaiMhs[i];
+                gagal++;
+            }
         }
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-        }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " + rata2);
+        rata2Lulus = totalLulus/lulus;
+        rata2Gagal = totalGagal/gagal;
+        System.out.println("Rata-rata nilai lulus : " + rata2Lulus);
+        System.out.println("Rata-rata nilai tidak lulus : " + rata2Gagal);
     }
 }
