cca698082920628df8a0f1264867e8ba40c2bc4d
zakariahaniya <zakalvha@gmail.com>
2024-10-30 09:23:57 +0700

    daspro jobsheet 9

---
+++ ArrayBilangan16.java
@@ -0,0 +1,17 @@
+/**
+ * ArrayBilangan16
+ */
+public class ArrayBilangan16 {
+
+    public static void main(String[] args) {
+        int[] bil = new int[4];
+        bil[0] = 5;
+        bil[1] = 13;
+        bil[2] = -7;
+        bil[3] = 17;
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+    }
+}
