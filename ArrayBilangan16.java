25f4fb2c2cecd34dc4015131b303bede01ddf73a
zakariahaniya <zakalvha@gmail.com>
2024-10-30 09:47:48 +0700

    daspro jobsheet 9

--- ArrayBilangan16.java
+++ ArrayBilangan16.java
@@ -4,14 +4,9 @@
 public class ArrayBilangan16 {
 
     public static void main(String[] args) {
-        int[] bil = new int[4];
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        int[] bil = {5, 13, -7, 17};
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
