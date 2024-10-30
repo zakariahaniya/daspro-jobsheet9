68f781cd63a7214dc8a8758a6b90d894380619e2
zakariahaniya <zakalvha@gmail.com>
2024-10-30 10:28:52 +0700

    daspro jobsheet 9

--- ArrayNilai16.java
+++ ArrayNilai16.java
@@ -7,12 +7,16 @@ public class ArrayNilai16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan Nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " lulus");   
+            } else {
+                System.out.println("Mahasiswa ke-" + i + "Tidak lulus");
+            }
         }
     }
 }
